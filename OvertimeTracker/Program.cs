42f0b45e5b6bcdc13a7c2175e8620899f674c9cf
Nicolas Röchter <rr@tobit.com>
2017-02-24 17:37:05 +0100

    Rename form

--- OvertimeTracker/Program.cs
+++ OvertimeTracker/Program.cs
@@ -13,7 +13,7 @@ namespace OvertimeTracker {
         static void Main () {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Overtime_Form());
         }
     }
 }
